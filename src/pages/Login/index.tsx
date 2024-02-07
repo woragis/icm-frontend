2386d1b80b0b424b1475bd2e99f15a13beca0d79
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 23:20:29 -0300

    Update Login page
    Create Submit Logic and connection to api.\nCreate Test Logic to see if email exists before letting user place password.\nImprove Semantic and Styling with GlobalStyles StyledComponents

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,10 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import {
-  StyledLogin,
-  StyledLoginElement,
-  StyledLoginForm,
-  StyledPasswordElement,
-} from "./style";
 import { animated } from "@react-spring/web";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { useSpring } from "@react-spring/web";
+import { LuAlertTriangle } from "react-icons/lu";
+import { Form, Field, Label, Main, Input, Button, Description, Title, Password, ShowPassword, ShowPasswordSpan, FormDialog } from "../../styles/GlobalStyles";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({
@@ -21,68 +17,142 @@ const Login = () => {
   };
 
   const handleLoginDataChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "email") setEmailTested(false);
     setLoginData({ ...loginData, [event.target.name]: event.target.value });
   };
 
   const emailPlaceholder = "nome@email.com";
   const passwordPlaceholder = "senha super secreta";
 
-  const handleSubmit = (event: FormEvent) => {
-    console.log(
-      "Sending login Data: email: " +
-        loginData.email +
-        " - password: " +
-        loginData.password
-    );
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/auth/login", {
+        method: "post",
+        cache: "default",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+        credentials: "include",
+      });
+
+      const data = await response.json();
+      if (data.message === "logged in") {
+        alert("logged in");
+      } else if (data.message === "wrong password") {
+        alert("wrong pasword");
+      } else {
+        alert(data.message);
+      }
+    } catch (err) {
+      console.log("Error submitting login " + err);
+    }
+    console.log("Sending login Data: email: " + loginData.email + " - password: " + loginData.password);
   };
 
-  const pageTransition = useSelector(
-    (state: RootState) => state.pageTranisition
-  );
+  const pageTransition = useSelector((state: RootState) => state.pageTranisition);
   const pageProps = useSpring(pageTransition);
 
+  const [emailExists, setEmailExists] = useState<boolean>(false);
+  const [emailTested, setEmailTested] = useState<boolean>(false);
+
+  const testEmailExsitance = async (event: any) => {
+    event.preventDefault();
+    const backendUri = "http://localhost:8080/auth/test-email";
+    try {
+      const response = await fetch(backendUri, {
+        method: "post",
+        cache: "default",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: loginData.email }),
+        credentials: "include",
+      });
+      interface DataInterface {
+        message: boolean;
+      }
+      const data: DataInterface = await response.json();
+      if (data.message === true) {
+        console.log("Email exists: " + data.message);
+        setEmailExists(true);
+      } else {
+        console.log("Email exists: " + data.message);
+        setEmailTested(true);
+      }
+    } catch (err) {
+      console.log("error testing email" + err);
+    }
+  };
+  const modifyEmail = (event: FormEvent) => {
+    event.preventDefault();
+    setEmailExists(false);
+    setEmailTested(false);
+  };
+
   return (
-    <animated.main style={pageProps}>
-      <StyledLoginForm onSubmit={(e) => handleSubmit(e)}>
-        <h1>Logar-se</h1>
-        <StyledLoginElement className="login-element">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={loginData.email}
-            placeholder={emailPlaceholder}
-            onChange={(e) => handleLoginDataChange(e)}
-          />
-        </StyledLoginElement>
-        <StyledLoginElement className="login-element">
-          <label htmlFor="password">Senha</label>
-          <StyledPasswordElement className="password-input">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              value={loginData.password}
-              placeholder={passwordPlaceholder}
-              id="password"
-              onChange={(e) => handleLoginDataChange(e)}
-            />
-            <input
-              type="checkbox"
-              name="show-password"
-              id="show-password"
-              checked={showPassword}
-              onClick={togglePassword}
-            />
-          </StyledPasswordElement>
-        </StyledLoginElement>
-        <div className="login-element">
-          <button type="submit" onSubmit={(e) => handleSubmit(e)}>
-            Login
-          </button>
-        </div>
-      </StyledLoginForm>
-    </animated.main>
+    <Main style={pageProps}>
+      <Form onSubmit={handleSubmit}>
+        <Title>Logar-se</Title>
+        {!emailExists ? (
+          <Field>
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" name="email" value={loginData.email} placeholder={emailPlaceholder} autoComplete="email" onChange={handleLoginDataChange} />
+          </Field>
+        ) : (
+          <Field>
+            <Label htmlFor="email">Email</Label>
+            <Description className="available-email">{loginData.email}</Description>
+          </Field>
+        )}
+        <Field>
+          {!emailExists ? (
+            <Button onClick={testEmailExsitance}>
+              Next
+              {emailTested && (
+                <FormDialog id="message">
+                  <LuAlertTriangle color="yellow" />
+                  <br />
+                  Aparentemente seu email nao existe no nosso banco de dados
+                  <br />
+                  <span>Registre-se ou tente outro email</span>
+                </FormDialog>
+              )}
+            </Button>
+          ) : (
+            <Button onClick={modifyEmail}>Modify Email</Button>
+          )}
+        </Field>
+        {emailExists && (
+          <>
+            <Field>
+              <Label htmlFor="password">Senha</Label>
+              <Password>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={loginData.password}
+                  placeholder={passwordPlaceholder}
+                  autoComplete="none"
+                  id="password"
+                  onChange={handleLoginDataChange}
+                />
+                <ShowPassword>
+                  <Input type="checkbox" name="show-password" checked={showPassword} onChange={togglePassword} />
+                  <ShowPasswordSpan />
+                </ShowPassword>
+              </Password>
+            </Field>
+            <Field>
+              <Button type="submit" onSubmit={handleSubmit}>
+                Login
+              </Button>
+            </Field>
+          </>
+        )}
+      </Form>
+    </Main>
   );
 };
 
