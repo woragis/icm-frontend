545f339a1da37dea2b3f701f8059d862101a46da
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 23:15:15 -0300

    Clean local styles because of GlobalStyles already has it

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,9 +1,1 @@
 import styled from "styled-components";
-
-export const StyledLogin = styled.main``;
-
-export const StyledLoginForm = styled.form``;
-
-export const StyledLoginElement = styled.div``;
-
-export const StyledPasswordElement = styled.div``;
